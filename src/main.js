706a1857623edee9326eb80683381b6d2952075b
MMARTID <miguelmartindelgado1998@gmail.com>
2024-11-27 16:32:49 +0100

    floor node

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ const gameScreenNode = document.getElementById("game-screen");
 const gameOverNode = document.getElementById("game-over-screen");
 const startBtnNode = document.querySelector("#start-btn");
 
+
+
+
+
+const gameFloor = document.createElement("div")
+gameFloor.id = "suelo";
+gameFloor.style.position = 'absolute';
+gameFloor.style.bottom = '0';
+gameFloor.style.left = '0';
+gameFloor.style.width = '100%';  
+gameFloor.style.height = '20px';  // Altura del suelo
+gameFloor.style.backgroundColor = '#654321';  
+gameFloor.style.zIndex = '1';
+
 startBtnNode.addEventListener("click", startGame);
 
 function startGame() {
   console.log("el juego ha empezado")  
   character = new Character();
+  gameScreenNode.appendChild(gameFloor)
     
 }
